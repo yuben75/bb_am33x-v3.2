6cda9fcbc3f8c2eb155c958fb7dfa923292857d6
yuben75 <yuben75@gmail.com>
2018-10-06 22:11:50 +0800

    TI OMAP_L138 ecap_cap driver

--- include/linux/pwm/pwm.h
+++ include/linux/pwm/pwm.h
@@ -20,24 +20,24 @@
 #define __LINUX_PWM_H
 
 enum {
-	FLAG_REGISTERED		= 0,
-	FLAG_REQUESTED		= 1,
-	FLAG_STOP		= 2,
-	FLAG_RUNNING		= 3,
+        FLAG_REGISTERED         = 0,
+        FLAG_REQUESTED          = 1,
+        FLAG_STOP               = 2,
+        FLAG_RUNNING            = 3,
 };
 
 enum {
-	PWM_CONFIG_DUTY_TICKS	= 0,
-	PWM_CONFIG_PERIOD_TICKS	= 1,
-	PWM_CONFIG_POLARITY	= 2,
-	PWM_CONFIG_START	= 3,
-	PWM_CONFIG_STOP		= 4,
+        PWM_CONFIG_DUTY_TICKS   = 0,
+        PWM_CONFIG_PERIOD_TICKS = 1,
+        PWM_CONFIG_POLARITY     = 2,
+        PWM_CONFIG_START        = 3,
+        PWM_CONFIG_STOP         = 4,
 
-	PWM_CONFIG_HANDLER	= 5,
+        PWM_CONFIG_HANDLER      = 5,
 
-	PWM_CONFIG_DUTY_NS	= 6,
-	PWM_CONFIG_DUTY_PERCENT	= 7,
-	PWM_CONFIG_PERIOD_NS	= 8,
+        PWM_CONFIG_DUTY_NS      = 6,
+        PWM_CONFIG_DUTY_PERCENT = 7,
+        PWM_CONFIG_PERIOD_NS    = 8,
 };
 
 struct pwm_config;
@@ -47,83 +47,62 @@ typedef int (*pwm_handler_t)(struct pwm_device *p, void *data);
 typedef void (*pwm_callback_t)(struct pwm_device *p);
 
 struct pwm_device_ops {
-	int	(*request)		(struct pwm_device *p);
-	void	(*release)		(struct pwm_device *p);
-	int	(*config)		(struct pwm_device *p,
-					 struct pwm_config *c);
-	int	(*config_nosleep)	(struct pwm_device *p,
-					 struct pwm_config *c);
-	int	(*synchronize)		(struct pwm_device *p,
-					 struct pwm_device *to_p);
-	int	(*unsynchronize)	(struct pwm_device *p,
-					 struct pwm_device *from_p);
-	int	(*set_callback)		(struct pwm_device *p,
-					 pwm_callback_t callback);
-	int	(*freq_transition_notifier_cb) (struct pwm_device *p);
+        int     (*request)              (struct pwm_device *p);
+        void    (*release)              (struct pwm_device *p);
+        int     (*config)               (struct pwm_device *p,
+                                         struct pwm_config *c);
+        int     (*config_nosleep)       (struct pwm_device *p,
+                                         struct pwm_config *c);
+        int     (*synchronize)          (struct pwm_device *p,
+                                         struct pwm_device *to_p);
+        int     (*unsynchronize)        (struct pwm_device *p,
+                                         struct pwm_device *from_p);
+        int     (*set_callback)         (struct pwm_device *p,
+                                         pwm_callback_t callback);
+        int     (*freq_transition_notifier_cb) (struct pwm_device *p);
 };
 
 struct pwm_config {
-	unsigned long config_mask;
-	unsigned long duty_ticks;
-	unsigned long period_ticks;
-	int polarity;
+        unsigned long config_mask;
+        unsigned long duty_ticks;
+        unsigned long period_ticks;
+        int polarity;
 
-	pwm_handler_t handler;
+        pwm_handler_t handler;
 
-	unsigned long duty_ns;
-	unsigned long period_ns;
-	int duty_percent;
+        unsigned long duty_ns;
+        unsigned long period_ns;
+        int duty_percent;
 };
 
 struct pwm_device {
-	struct list_head list;
+        struct list_head list;
 
-	struct device *dev;
-	struct pwm_device_ops *ops;
+        struct device *dev;
+        struct pwm_device_ops *ops;
 
-	void *data;
+        void *data;
 
-	const char *label;
-	pid_t pid;
+        const char *label;
+        pid_t pid;
 
-	volatile unsigned long flags;
+        volatile unsigned long flags;
 
-	unsigned long tick_hz;
+        unsigned long tick_hz;
 
-	pwm_callback_t callback;
+        pwm_callback_t callback;
 
-	struct work_struct handler_work;
-	pwm_handler_t handler;
-	void *handler_data;
+        struct work_struct handler_work;
+        pwm_handler_t handler;
+        void *handler_data;
 
-	int active_high;
-	unsigned long period_ticks;
-	unsigned long duty_ticks;
-	unsigned long period_ns;
-	unsigned long duty_ns;
-	struct notifier_block freq_transition;
-	unsigned long max_period_ticks;
-	spinlock_t pwm_lock;
-};
-
-#include <linux/semaphore.h>
-#include <linux/pwm/ehrpwm.h>
-
-enum {
-	PWM_VERSION_0,
-	PWM_VERSION_1,
-};
-
-struct pwm_chan_attrib {
-	int max_freq;
-};
-
-#define PWM_CHANNEL NCHAN
-
-struct pwmss_platform_data {
-	int channel_mask;
-	u8 version;
-	struct pwm_chan_attrib chan_attrib[PWM_CHANNEL];
+        int active_high;
+        unsigned long period_ticks;
+        unsigned long duty_ticks;
+        unsigned long period_ns;
+        unsigned long duty_ns;
+        struct notifier_block freq_transition;
+        spinlock_t pwm_lock;
 };
 
 struct pwm_device *pwm_request_byname(const char *name, const char *label);
@@ -132,27 +111,27 @@ void pwm_release(struct pwm_device *p);
 
 static inline int pwm_is_registered(struct pwm_device *p)
 {
-	return test_bit(FLAG_REGISTERED, &p->flags);
+        return test_bit(FLAG_REGISTERED, &p->flags);
 }
 
 static inline int pwm_is_requested(struct pwm_device *p)
 {
-	return test_bit(FLAG_REQUESTED, &p->flags);
+        return test_bit(FLAG_REQUESTED, &p->flags);
 }
 
 static inline int pwm_is_running(struct pwm_device *p)
 {
-	return test_bit(FLAG_RUNNING, &p->flags);
+        return test_bit(FLAG_RUNNING, &p->flags);
 }
 
 static inline void pwm_set_drvdata(struct pwm_device *p, void *data)
 {
-	p->data = data;
+        p->data = data;
 }
 
 static inline void *pwm_get_drvdata(const struct pwm_device *p)
 {
-	return p->data;
+        return p->data;
 }
 
 unsigned long pwm_ns_to_ticks(struct pwm_device *p, unsigned long nsecs);
@@ -163,6 +142,7 @@ int pwm_config(struct pwm_device *p, struct pwm_config *c);
 
 int pwm_set_period_ns(struct pwm_device *p, unsigned long period_ns);
 unsigned long pwm_get_period_ns(struct pwm_device *p);
+
 int pwm_set_duty_ns(struct pwm_device *p, unsigned long duty_ns);
 unsigned long pwm_get_duty_ns(struct pwm_device *p);
 int pwm_set_duty_percent(struct pwm_device *p, int percent);
@@ -177,7 +157,7 @@ int pwm_set_handler(struct pwm_device *p, pwm_handler_t handler, void *data);
 
 int pwm_register(struct pwm_device *p, struct device *parent, int id);
 int pwm_register_byname(struct pwm_device *p, struct device *parent,
-			const char *name);
+                        const char *name);
 int pwm_unregister(struct pwm_device *p);
 
 #ifdef CONFIG_GPIO_PWM
@@ -187,8 +167,10 @@ int gpio_pwm_destroy(struct pwm_device *p);
 int pwm_set_frequency(struct pwm_device *p, unsigned long freq);
 unsigned long pwm_get_frequency(struct pwm_device *p);
 int pwm_set_period_ticks(struct pwm_device *p,
-					unsigned long ticks);
+                                        unsigned long ticks);
 unsigned long pwm_get_duty_percent(struct pwm_device *p);
 int pwm_set_duty_ticks(struct pwm_device *p,
-					unsigned long ticks);
+                                        unsigned long ticks);
+struct device *capture_request_device(char *name);
+struct device *capture_dev_register(struct device *dev);
 #endif
